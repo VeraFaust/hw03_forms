cb2de5cf7a06969e25d728fab5d47c45c8c3c0ec
VeraFaust <110284629+VeraFaust@users.noreply.github.com>
2023-11-24 19:54:01 +0300

    Update urls.py

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,10 +5,16 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
+    # Группа
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
+    # Профайл
     path('profile/<str:username>/', views.profile, name='profile'),
+    # Редактирование поста
     path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    # Пост
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    # Создание поста
     path('create/', views.post_create, name='post_create'),
+    # главная страница
     path('', views.index, name='index'),
 ]
